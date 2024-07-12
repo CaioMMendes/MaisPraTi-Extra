324cc1621bbb6075d7134e83623fdf54477d32ed
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2024-07-12 15:01:32 -0300

    feat: Add ToggleText

---
+++ Atividade01-CicloDeVida/src/exercises/ToggleText.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react"
+import { Button } from "../components/button"
+
+const ToggleText = () => {
+  const [isVisible, setIsVisible] = useState(true)
+
+  function handleChangeVisible() {
+    setIsVisible((visible) => !visible)
+  }
+
+  return (
+    <section className="flex flex-col gap-2">
+      <h1 className="font-medium text-lg">Exibir/Mostrar Texto</h1>
+      <div className="flex gap-4 items-start flex-col justify-center">
+        <Button onClick={handleChangeVisible}>
+          Tornar {isVisible ? "Invisível" : "Visível"}
+        </Button>
+        {isVisible && <p>O texto está visível</p>}
+      </div>
+    </section>
+  )
+}
+
+export default ToggleText
